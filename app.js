7286daaccee8e73d0155db82043c6c1812283357
Sifa <sifazhang.nz@gmail.com>
2025-04-25 17:58:23 +1200

    delete node folder

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ console.log("Application is starting...");
 const express = require('express');
 const app = express();
 const port = 3000;
+const ejs = require('ejs');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 
 
 app.get("/",(req, res, next) => {
     console.log("I have received a express.request");
-        res.send("<h1>Thank your for your request</h1>");
+    let html = ejs.render("homepage.ejs");
+        res.send(html);
 });
+
+app.get("/welcome",(req, res, next) => {
+    console.log("I have received a express.request");
+    res.send("<h1>Welcome to the express application</h1>");
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
